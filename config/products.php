11ce24a282dbcdae6096df61934680bdf1cd3e22
Yaroslav Ostapuk <y.ostapuk@ideil.com>
2022-12-29 10:11:21 +0200

    Update products config

--- config/products.php
+++ config/products.php
@@ -2,96 +2,95 @@
 
 return [
     'list' => [
-//        [
-//            'name' => 'S-Liner Pro',
-//            'url' => 'https://sviato.shop/product/s-liner-pro',
-//            'price' => 2125,
-//            'img' => [
-//                '1x' => 'images/products/',
-//                '2x' => 'images/products/',
-//            ],
-//        ],
-//        test
         [
-            'name' => 'PMU Sun Protect, SPF30',
-            'url' => 'https://sviato.shop/product/s-liner-basic',
-            'price' => 20,
+            'name' => 'S-Liner Pro',
+            'url' => 'https://sviato.shop/product/s-liner-pro',
+            'price' => 2125,
             'img' => [
-                '1x' => 'product-01.png',
-                '2x' => 'product-01.png',
+                '1x' => 'images/products/s_liner_pro.png',
+                '2x' => 'images/products/s_liner_pro@2x.png',
             ],
         ],
         [
             'name' => 'S-Liner Basic',
             'url' => 'https://sviato.shop/product/s-liner-basic',
-            'price' => 1000,
+            'price' => 900,
             'img' => [
-                '1x' => 'product-00.png',
-                '2x' => 'product-00.png',
+                '1x' => 'images/products/s_liner_basic.png',
+                '2x' => 'images/products/s_liner_basic@2x.png',
             ],
         ],
-//        [
-//            'name' => 'S-Liner Training',
-//            'url' => 'https://sviato.shop/product/s-liner-training',
-//            'price' => 250,
-//            'img' => [
-//                '1x' => 'images/products/',
-//                '2x' => 'images/products/',
-//            ],
-//        ],
-//        [
-//            'name' => 'LineS Collection 10x10ml',
-//            'url' => 'https://sviato.shop/product/lines-collection-10x10ml',
-//            'price' => 520,
-//            'img' => [
-//                '1x' => 'images/products/',
-//                '2x' => 'images/products/',
-//            ],
-//        ],
-//        [
-//            'name' => 'Magic Liner red (1pc)',
-//            'url' => 'https://sviato.shop/product/academys-magic-liner-red-1pc',
-//            'price' => 12,
-//            'img' => [
-//                '1x' => 'images/products/',
-//                '2x' => 'images/products/',
-//            ],
-//        ],
-//        [
-//            'name' => 'Magic Liner black (1pc)',
-//            'url' => 'https://sviato.shop/product/academys-magic-liner-black-1pc',
-//            'price' => 12,
-//            'img' => [
-//                '1x' => 'images/products/',
-//                '2x' => 'images/products/',
-//            ],
-//        ],
+        [
+            'name' => 'S-Liner Training',
+            'url' => 'https://sviato.shop/product/s-liner-training',
+            'price' => 225,
+            'img' => [
+                '1x' => 'images/products/s_liner_training.png',
+                '2x' => 'images/products/s_liner_training@2x.png',
+            ],
+        ],
+        [
+            'name' => 'LineS Collection 10x10ml',
+            'url' => 'https://sviato.shop/product/lines-collection-10x10ml',
+            'price' => 465,
+            'img' => [
+                '1x' => 'images/products/line_s_collection.png',
+                '2x' => 'images/products/line_s_collection@2x.png',
+            ],
+        ],
+        [
+            'name' => 'Magic Liner red (1pc)',
+            'url' => 'https://sviato.shop/product/academys-magic-liner-red-1pc',
+            'price' => 10.80,
+            'img' => [
+                '1x' => 'images/products/magic_liner_red.png',
+                '2x' => 'images/products/magic_liner_red@2x.png',
+            ],
+        ],
+        [
+            'name' => 'Magic Liner black (1pc)',
+            'url' => 'https://sviato.shop/product/academys-magic-liner-black-1pc',
+            'price' => 10.80,
+            'img' => [
+                '1x' => 'images/products/s_liner_magic_liner_black.png',
+                '2x' => 'images/products/s_liner_magic_liner_black@2x.png',
+            ],
+        ],
         [
             'name' => 'AfterS After Care Cream, 10ml',
-            'url' => 'https://sviato.shop/product/sviato-collection-black-luxury-uniform',
-            'price' => 133,
+            'url' => 'https://sviato.shop/product/afters-10ml/',
+            'price' => 9,
             'img' => [
-                '1x' => 'product-02.png',
-                '2x' => 'product-02.png',
+                '1x' => 'images/products/s_liner_after_s.png',
+                '2x' => 'images/products/s_liner_after_s@2x.png',
             ],
         ],
-//        [
-//            'name' => 'Sviato Collection Red Luxury Uniform',
-//            'url' => 'https://sviato.shop/product/sviato-collection-red-luxury-uniform',
-//            'price' => 180,
-//            'img' => [
-//                '1x' => 'images/products/',
-//                '2x' => 'images/products/',
-//            ],
-//        ],
-//        [
-//            'name' => 'Sviato Collection Black Hoodie',
-//            'url' => 'https://sviato.shop/product/sviato-collection-black-hoodie',
-//            'price' => 99,
-//            'img' => [
-//                '1x' => 'images/products/',
-//                '2x' => 'images/products/',
-//            ],
-//        ],
+        [
+            'name' => 'Sviato Collection Black Luxury Uniform',
+            'url' => 'https://sviato.shop/product/sviato-collection-red-luxury-uniform',
+            'price' => 117,
+            'img' => [
+                '1x' => 'images/products/s_liner_luxury_uniform_black.png',
+                '2x' => 'images/products/s_liner_luxury_uniform_black@2x.png',
+            ],
+        ],
+        [
+            'name' => 'Sviato Collection Red Luxury Uniform',
+            'url' => 'https://sviato.shop/product/sviato-collection-red-luxury-uniform',
+            'price' => 162,
+            'img' => [
+                '1x' => 'images/products/s_liner_luxury_uniform_red.png',
+                '2x' => 'images/products/s_liner_luxury_uniform_red@2x.png',
+            ],
+        ],
+        [
+            'name' => 'Sviato Collection Black Hoodie',
+            'url' => 'https://sviato.shop/product/sviato-collection-black-hoodie',
+            'price' => 89.10,
+            'img' => [
+                '1x' => 'images/products/s_liner_hoodie_black.png',
+                '2x' => 'images/products/s_liner_hoodie_black@2x.png',
+            ],
+        ],
     ],
 ];
