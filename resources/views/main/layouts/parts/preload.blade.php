a6632a84d3b28b736ceaffc499b65082e1ab9325
Yaroslav Ostapuk <y.ostapuk@ideil.com>
2022-12-30 11:14:40 +0200

    Add preload fonts and images

---
+++ resources/views/main/layouts/parts/preload.blade.php
@@ -0,0 +1,7 @@
+<link rel="preload" href="{{ Vite::fonts('BebasNeuePro-Bold.woff2') }}" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="{{ Vite::fonts('BebasNeuePro-BoldItalic.woff2') }}" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="{{ Vite::fonts('BebasNeuePro-Regular.woff2') }}" as="font" type="font/woff2" crossorigin>
+
+<link rel="preload" media="(max-width: 420px)" imagesrcset="{{ Vite::image('hero-00-420px.png') }} 1x, {{ Vite::image('hero-00-420px@2x.png') }} 2x" as="image">
+<link rel="preload" media="(min-width: 421px) and (max-width: 1024px)" imagesrcset="{{ Vite::image('hero-00-1024px.png') }} 1x, {{ Vite::image('hero-00-1024px@2x.png') }} 2x" as="image">
+<link rel="preload" media="(min-width: 1025px)" imagesrcset="{{ Vite::image('hero-00.png') }} 1x, {{ Vite::image('hero-00@2x.png') }} 2x" as="image">
