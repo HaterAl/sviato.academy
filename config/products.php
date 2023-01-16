dde4cb2eda87427f40aac76c98867065dd63230d
Yaroslav Ostapuk <y.ostapuk@ideil.com>
2023-01-16 11:36:57 +0200

    Update product prices

--- config/products.php
+++ config/products.php
@@ -5,7 +5,7 @@ return [
         [
             'name' => 'S-Liner Pro',
             'url' => 'https://sviato.shop/product/s-liner-pro',
-            'price' => 2125,
+            'price' => 2500,
             'img' => [
                 '1x' => 'images/products/s_liner_pro.png',
                 '2x' => 'images/products/s_liner_pro@2x.png',
@@ -14,7 +14,7 @@ return [
         [
             'name' => 'S-Liner Basic',
             'url' => 'https://sviato.shop/product/s-liner-basic',
-            'price' => 900,
+            'price' => 1000,
             'img' => [
                 '1x' => 'images/products/s_liner_basic.png',
                 '2x' => 'images/products/s_liner_basic@2x.png',
@@ -23,7 +23,7 @@ return [
         [
             'name' => 'S-Liner Training',
             'url' => 'https://sviato.shop/product/s-liner-training',
-            'price' => 225,
+            'price' => 250,
             'img' => [
                 '1x' => 'images/products/s_liner_training.png',
                 '2x' => 'images/products/s_liner_training@2x.png',
@@ -32,16 +32,16 @@ return [
         [
             'name' => 'LineS Collection 10x10ml',
             'url' => 'https://sviato.shop/product/lines-collection-10x10ml',
-            'price' => 465,
+            'price' => 520,
             'img' => [
-                '1x' => 'images/products/line_s_collection.png',
-                '2x' => 'images/products/line_s_collection@2x.png',
+                '1x' => 'images/products/line_s_collection.png?v=2',
+                '2x' => 'images/products/line_s_collection@2x.png?v=2',
             ],
         ],
         [
             'name' => 'Magic Liner red (1pc)',
             'url' => 'https://sviato.shop/product/academys-magic-liner-red-1pc',
-            'price' => 10.80,
+            'price' => 12,
             'img' => [
                 '1x' => 'images/products/magic_liner_red.png',
                 '2x' => 'images/products/magic_liner_red@2x.png',
@@ -50,7 +50,7 @@ return [
         [
             'name' => 'Magic Liner black (1pc)',
             'url' => 'https://sviato.shop/product/academys-magic-liner-black-1pc',
-            'price' => 10.80,
+            'price' => 12,
             'img' => [
                 '1x' => 'images/products/s_liner_magic_liner_black.png',
                 '2x' => 'images/products/s_liner_magic_liner_black@2x.png',
@@ -59,7 +59,7 @@ return [
         [
             'name' => 'AfterS After Care Cream, 10ml',
             'url' => 'https://sviato.shop/product/afters-10ml/',
-            'price' => 9,
+            'price' => 10,
             'img' => [
                 '1x' => 'images/products/s_liner_after_s.png',
                 '2x' => 'images/products/s_liner_after_s@2x.png',
@@ -68,7 +68,7 @@ return [
         [
             'name' => 'Sviato Collection Black Luxury Uniform',
             'url' => 'https://sviato.shop/product/sviato-collection-black-luxury-uniform',
-            'price' => 117,
+            'price' => 130,
             'img' => [
                 '1x' => 'images/products/s_liner_luxury_uniform_black.png',
                 '2x' => 'images/products/s_liner_luxury_uniform_black@2x.png',
@@ -77,7 +77,7 @@ return [
         [
             'name' => 'Sviato Collection Red Luxury Uniform',
             'url' => 'https://sviato.shop/product/sviato-collection-red-luxury-uniform',
-            'price' => 162,
+            'price' => 180,
             'img' => [
                 '1x' => 'images/products/s_liner_luxury_uniform_red.png',
                 '2x' => 'images/products/s_liner_luxury_uniform_red@2x.png',
@@ -86,7 +86,7 @@ return [
         [
             'name' => 'Sviato Collection Black Hoodie',
             'url' => 'https://sviato.shop/product/sviato-collection-black-hoodie',
-            'price' => 89.10,
+            'price' => 45,
             'img' => [
                 '1x' => 'images/products/s_liner_hoodie_black.png',
                 '2x' => 'images/products/s_liner_hoodie_black@2x.png',
